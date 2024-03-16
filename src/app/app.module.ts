5f4ae5790e4f9effe85921f013f252ce8f93a923
mssantostuc <mssantostuc@gmail.com>
2024-03-16 07:24:04 -0300

    Componente HEADER criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,11 +29,15 @@ import { MatCardModule } from '@angular/material/card';
 
 //Componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
