0659750b4aef587c24469487266b4658af06b402
mssantostuc <mssantostuc@gmail.com>
2024-03-25 05:55:55 -0300

    Interceptor para requisições HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 //import { ToastrModule } from 'ng6-toastr-notifications';
 
 
@@ -79,7 +80,7 @@ import { ToastrModule } from 'ngx-toastr';
     }),
      
   ],
-  providers: [],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
