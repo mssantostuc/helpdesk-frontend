8f36f96d69ee49ebbd92a9b03cedaf529d7c2df6
mssantostuc <mssantostuc@gmail.com>
2024-03-30 10:25:19 -0300

    Component update implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 //import { ToastrModule } from 'ng6-toastr-notifications';
 import { NgxMaskModule } from 'ngx-mask';
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 
 @NgModule({
@@ -50,6 +51,7 @@ import { NgxMaskModule } from 'ngx-mask';
     TecnicoListComponent,
     LoginComponent,
     TecnicoCreateComponent,
+    TecnicoUpdateComponent,
     
   ],
   imports: [
