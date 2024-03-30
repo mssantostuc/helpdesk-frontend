5db3f07fb2aba7522ad4f7edccc8e80cc9484c30
mssantostuc <mssantostuc@gmail.com>
2024-03-30 14:03:58 -0300

    Component tecnico-delete finalizado

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -11,23 +11,25 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let token = localStorage.getItem('token');
 
-    if(token){
-      const cloneReq = request.clone({ headers: request.headers.set('Authorization',`Bearer ${token}`)});
-      return next.handle(cloneReq);
-    }else{
+    if (token) {
+      const cloneReq = 
+        request.clone({ headers: request.headers.set('Authorization', `Bearer ${token}`) });
+        return next.handle(cloneReq);
+    } else {
       return next.handle(request);
     }
-    
   }
 }
 
-export const AuthInterceptorProvider = {
-  provide: HTTP_INTERCEPTORS,
-  useClass: AuthInterceptor,
-  multi: true
-}
+export const AuthInterceptorProvider = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }
+]
